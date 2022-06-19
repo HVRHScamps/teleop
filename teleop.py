a36f99000c5a3a80468ffa635a069d712020c633
cannotilever <51347140+cannotilever@users.noreply.github.com>
2022-06-19 17:41:02 -0400

    remove separate controller variable

--- teleop.py
+++ teleop.py
@@ -1,5 +1,5 @@
 import libhousy
 #You can define helper functions here, make sure to but them *above* the main function
-def main(robot: libhousy.robot, controller: libhousy.controller):
+def main(robot: libhousy.robot):
     #Here is where your recurring code will go
     print("Hello World!")
