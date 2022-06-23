83f3850e8f78f177a5878fcc1cbd259ebfbf1971
Braian-perez <bperez527@hotmail.com>
2022-06-23 11:48:43 -0400

    upload

--- teleop.py
+++ teleop.py
@@ -2,19 +2,15 @@ import libhousy
 #You can define helper functions here, make sure to but them *above* the main function
 def main(robot: libhousy.robot):
     #Here is where your recurring code will go
-    print("Hello World!")
-    if  robot.controller.getAxis(robot.controller.Axis.rStickX) >0.8:
-        robot.rDrive.Set(-0.7)
-        robot.lDrive.Set(0.7)
-    elif robot.controller.getAxis(robot.controller.Axis.rStickX) <-0.8:
-        robot.rDrive.Set(0.7)
-        robot.lDrive.Set(-0.7)
-    elif robot.controller.getAxis(robot.controller.Axis.lStickY) >0.8:
-        robot.rDrive.Set(0.7)
-        robot.lDrive.Set(0.7)
-    elif robot.controller.getAxis(robot.controller.Axis.lStickY) <-0.8:
-        robot.rDrive.Set(-0.7)
-        robot.lDrive.Set(-0.7)
-    else: 
-        robot.rDrive.Set(0)
-        robot.lDrive.Set(0)
+    left_throttle = robot.controller.getAxis(robot.controller.Axis.lStickY) + robot.controller.getAxis(robot.controller.Axis.lStickX)
+    right_throttle =robot.controller.getAxis(robot.controller.Axis.lStickY) - robot.controller.getAxis(robot.controller.Axis.lStickX)
+    if left_throttle >1:
+        left_throttle=1
+    if right_throttle>1:
+        right_throttle=1
+    if  left_throttle<-1:
+        left_throttle=-1
+    if right_throttle<-1:
+        right_throttle=-1
+    robot.lDrive.Set(left_throttle)
+    robot.rDrive.Set(right_throttle)
