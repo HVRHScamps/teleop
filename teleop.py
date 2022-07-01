96e3d737fdb9ace09e02f0cc6092870e5ad14196
Logan <loganpadelli@gmail.com>
2022-07-01 10:04:28 -0400

    Auto pickup portion

--- teleop.py
+++ teleop.py
@@ -1,9 +1,24 @@
 # inclue code to help us talk to the robot
 import libhousy
 
+def pickup(robot):
+    print("running pickup mode")
+    robot.pickupMotor.Set(1)
+    robot.pickupPneumatic.Extend()
+    robot.beltZ1.Set(-0.8)
+    robot.beltZ2.Set(0)
+    robot.beltZ3.Set(0)
+    robot.upperTension.Retract()
+    robot.lowerTension.Extend()
+
 def main(robot: libhousy.robot):
     # Here is where your recurring code will go
-    print("Hello World!")
-    
-    # After everything is done, we tell the main program to stop us
-    return libhousy.DONE
+    if robot.controller.getAxis(robot.controller.Axis.lTrigger) > 0.8:
+        pickup(robot)
+   
+    else:
+        robot.pickupMotor.Set(0)
+        robot.pickupPneumatic.Retract()
+        robot.beltZ1.Set(0)
+        robot.beltZ2.Set(0)
+        robot.beltZ3.Set(0)
