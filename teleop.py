463706a3030b938679c91a0e7ef2660558bc5382
Whackyman <dieselmntbiker69@outlook.com>
2022-07-01 10:01:12 -0400

    hold still from controller

--- teleop.py
+++ teleop.py
@@ -1,9 +1,42 @@
 # inclue code to help us talk to the robot
 import libhousy
 
+def holdStill(robot):
+    def main(robot: libhousy.robot):
+    
+        if robot.controller.getButton(robot.controller.Button.x):
+            first = True
+
+p = 0.05
 def main(robot: libhousy.robot):
-    # Here is where your recurring code will go
-    print("Hello World!")
+    global first
+    if first:
+        robot.rDriveEncoder.Reset()
+        robot.lDriveEncoder.Reset()
+        first = False 
+
+    if robot.rDriveEncoder.Get() > 2:
+        error = 0.0 - robot.rDriveEncoder.Get()
+        speed = error * p 
+        robot.rDrive.Set(speed)
+    elif robot.rDriveEncoder.Get() < -2:
+        error = 0.0 - robot.rDriveEncoder.Get()
+        speed = error * p 
+        robot.rDrive.Set(speed)
+    else: 
+        robot.rDrive.Set(0)
+
+    if robot.lDriveEncoder.Get() > 2:
+        robot.lDrive.Set(-.5)
+        error = 0.0 - robot.lDriveEncoder.Get()
+        speed = error * p
+        robot.lDrive.Set(speed)
+    elif robot.lDriveEncoder.Get() < -2:
+        error = 0.0 - robot.lDriveEncoder.Get()
+        speed = error * p
+        robot.lDrive.Set(speed)
+    else:
+        robot.lDrive.Set(0)
     
     # After everything is done, we tell the main program to stop us
-    return libhousy.DONE
+    #return libhousy.DONE
