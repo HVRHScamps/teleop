0cb29f732a5a01b44bf12fcf1fad4691fb69aeda
Lily <24danforthgld@cornwallschool.org>
2022-07-01 10:42:02 -0400

    add drive

--- teleop.py
+++ teleop.py
@@ -1,9 +1,21 @@
 # inclue code to help us talk to the robot
+from lib2to3.pgen2.token import RPAR
 import libhousy
 
 def main(robot: libhousy.robot):
     # Here is where your recurring code will go
-    print("Hello World!")
-    
-    # After everything is done, we tell the main program to stop us
-    return libhousy.DONE
+    stickY = robot.controller.getAxis(robot.controller.Axis.lStickY)
+    stickX = robot.controller.getAxis(robot.controller.Axis.lStickX)
+    lPower = stickY+stickX
+    rPower = stickY-stickX
+    if lPower > 1: 
+        lPower = 1
+    if rPower > 1:
+        rPower = 1
+    if lPower < -1: 
+        lPower = -1
+    if rPower < -1:
+        rPower = -1
+        
+    robot.lDrive.Set(lPower)
+    robot.rDrive.Set(rPower)
